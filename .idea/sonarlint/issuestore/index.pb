ef333e58c7ee36f66f5171d3fac0ce13f70274d3
test133nick <rakovnd133@gmail.com>
2023-01-19 10:05:58 +0300

    no message

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+9
+	Check.bat,8\5\85f6892a5e2ac64f3c1d6347097f04baf429b619
---
+++ Check.bat
@@ -0,0 +1,1 @@
+sonar-scanner.bat -D"sonar.projectKey=Json5" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9005" -D"sonar.login=sqp_87c9b948d4a9af8e60abfce8c56345ccbadf0ef0"
